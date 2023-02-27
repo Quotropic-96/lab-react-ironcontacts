18644d68fa4a9711b52af492dc58b5e1d1734d22
Gerard Solanes <gerardsolher@gmail.com>
2023-02-27 15:46:18 +0100

    1st iteration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import "./App.css";
+import React, { useState } from "react";
+import contactsDB from './contacts.json'
 
 function App() {
-  return <div className="App"></div>;
+  const [contacts, setContacts] = useState(contactsDB);
+  return <div className="App">
+    <h1>IronContacts</h1>
+    <table>
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Popularity</th>
+      </tr>
+      {contacts.map((elem, idx) => idx < 4 ? <tr key={idx}><td><img src={elem.pictureUrl} alt="Profile picture" /></td><td>{elem.name}</td><td>{elem.popularity}</td></tr> : null)}
+    </table>
+  </div>;
 }
 export default App;
