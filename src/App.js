7bfd53bd20e006e086c6bc501e8536a80eca20db
Gerard Solanes <gerardsolher@gmail.com>
2023-02-27 16:02:28 +0100

    Complete iteration 3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import React, { useState } from "react";
 import contactsDB from './contacts.json'
 
 function App() {
-  const [contacts, setContacts] = useState(contactsDB);
+  const [contacts, setContacts] = useState(contactsDB.slice(0,5));
+
+  const handleAddRandomStar = () => {
+    const randomContacts = [...contacts]
+    randomContacts.push(contactsDB[Math.floor(Math.random() * (contactsDB.length - 5) + 5)]);
+    setContacts(randomContacts);
+  }
   return <div className="App">
     <h1>IronContacts</h1>
     <table>
@@ -14,15 +20,17 @@ function App() {
         <th>Oscar</th>
         <th>Emmy</th>
       </tr>
-      {contacts.map((elem, idx) => idx <= 4 ? 
-        <tr key={idx}>
-          <td><img src={elem.pictureUrl} alt="Profile picture" /></td>
-          <td>{elem.name}</td>
-          <td>{elem.popularity}</td>
-          <td>{elem.wonOscar && <span>🏆</span>}</td>
-          <td>{elem.wonEmmy && <span>🏆</span>}</td>
-        </tr> : null)}
+      {contacts.map((elem, idx) => 
+          <tr key={idx}>
+            <td><img src={elem.pictureUrl} alt="Profile" /></td>
+            <td>{elem.name}</td>
+            <td>{elem.popularity}</td>
+            <td>{elem.wonOscar && <span>🏆</span>}</td>
+            <td>{elem.wonEmmy && <span>🏆</span>}</td>
+          </tr>
+        )}
     </table>
+    <button className="btn" onClick={handleAddRandomStar}>Add random star</button>
   </div>;
 }
 export default App;
